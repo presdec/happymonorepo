aed6ee2d2def0ac265f6d7a9d56f0d482d769200
presdec <presdec@gmail.com>
2023-07-15 23:49:27 +0200

    sorta stable

--- libs/ui/.storybook/main.ts
+++ libs/ui/.storybook/main.ts
@@ -1,6 +1,8 @@
-const config = {
-  stories: ['../src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials'],
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  stories: ['../src/components/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-styling'],
   framework: {
     name: '@storybook/react-vite',
     options: {
